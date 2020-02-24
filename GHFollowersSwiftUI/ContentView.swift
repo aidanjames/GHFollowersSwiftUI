a78bb55fb9a0df9f013ed4a785864aac37ea9ccc
AidanJP <aidanpendlebury@gmail.com>
2020-02-24 19:47:22 +0000

    Initial Commit

---
+++ GHFollowersSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GHFollowersSwiftUI
+//
+//  Created by Aidan Pendlebury on 24/02/2020.
+//  Copyright © 2020 Aidan Pendlebury. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
